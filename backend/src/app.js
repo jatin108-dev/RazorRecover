f5b5b815b94579a06052daadde1cc86f17913413
Portfolio <jatindubey646@gmail.com>
2026-08-29 20:43:00 +0530

    implemented cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const env = require('dotenv').config();
+const cookieParser = require("cookie-parser");
 
 const webhookRoutes = require("./routes/webhookRoutes");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 app.use("/api/webhooks", webhookRoutes);
